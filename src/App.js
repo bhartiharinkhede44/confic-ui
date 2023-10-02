5e1021101a305b753740c28b6feccee8a7661447
bhartiharinkhede44 <bhartiharinkhede44@gmail.com>
2023-10-02 16:39:22 +0530

    ui added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import Config from "./util/Config.json";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='card-container' >
+     <h1 className='heading' style={{color: Config.theme.primaryColor}}>{Config.title}</h1>
+     <p>{Config.description}</p>
+     
+     {
+      Config.services.map((servicesData,index)=>{
+        const{title,description,timing}=servicesData
+        return(
+          <div className='card' key={index}>
+            <div className='card-body'>
+              <h3 className='Card-title' style={{color: Config.theme.secondaryColor}} >{title}</h3>
+              <p className='Card-text'>{description}</p>
+              <p className='Card-text'>{timing}</p></div>
+
+          </div>
+        )
+      })
+     }
+      
+      </div>
   );
 }
 
